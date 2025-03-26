c873739e88054de885a8d58896db5f43909a243b
Zver <andreevdamir266@gmail.com>
2025-03-26 18:05:54 +0300

    commit2

---
+++ school/src/main/java/ru/hogwarts/school/services/impl/AvatarServiceImpl.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.services.impl;
+
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+
+import java.io.IOException;
+
+public interface AvatarServiceImpl {
+
+    void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException;
+
+    Avatar findAvatar(Long studentId);
+}
