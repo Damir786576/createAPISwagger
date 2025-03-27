fda913cbff19cf62eba26a687bbe495ce987e5ae
Zver <andreevdamir266@gmail.com>
2025-03-27 15:20:09 +0300

    управление схемами баз данных

---
+++ school/ scripts421.sql
@@ -0,0 +1,7 @@
+ALTER TABLE student
+ADD CONSTRAINT age_constraint CHECK ( age > 16 ),
+ALTER COLUMN name SET NOT NULL,
+ADD CONSTRAINT name_unique UNIQUE (name),
+ALTER COLUMN name SET DEFAULT 20;
+ALTER TABLE faculty
+CONSTRAINT faculty_name_unique UNIQUE (name, colour)
